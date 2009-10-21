d467baef83570cde82169eacbf4ccc3b5f3d916c
lestrrat <lestrrat+github@gmail.com>
2009-10-21 14:12:09 +0900

    allow traversal

--- lib/Gitcat/Web/Controller/Tree.pm
+++ lib/Gitcat/Web/Controller/Tree.pm
@@ -10,14 +10,16 @@ sub view
     my ($self, $c, @comps) = @_;
 
     my $repo   = $c->stash->{repo};
+    my $branch = $c->stash->{branch};
     my $object = @comps > 0 ? 
-        $repo->get_object_from_path($c->stash->{branch_id}, @comps) :
-        $repo->get_object($c->stash->{branch}->sha1)->tree
+        $repo->get_object_from_path($branch->sha1, @comps) :
+        $repo->get_object($branch->sha1)->tree
     ;
 
     # switch template depending on the object type
     $c->stash(
         object => $object,
+        tree_comp => \@comps,
         template => sprintf('tree/%s.tt', $object->kind),
     );
 }
